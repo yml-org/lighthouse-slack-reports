7940c9abb2f1e763f116cc1ec882bb654f79ba3c
Edwin Pradeep <edwin.pradeep@ymedialabs.com>
2022-08-18 18:25:32 +0530

    lighthouserc.ts and .js added to gitignore

--- lighthouserc.ts
+++ lighthouserc.ts
@@ -1,6 +1,6 @@
 module.exports = {
-	slackSigningSecret: '468b2ead8ad96c90d87df9adf77352ca',
-	slackWebhookUrl: 'https://hooks.slack.com/services/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',
+	slackSigningSecret: '',
+	slackWebhookUrl: '',
 	urlsToProfile: [
 		'https://develop.d337wy32ueusth.amplifyapp.com/',
 		'https://develop.d337wy32ueusth.amplifyapp.com/work',
